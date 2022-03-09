1ef9fd9c8a1a29467fcd37b6200a831e3e3ed501
Jesus <jesus9449ruiz@gmail.com>
2022-03-09 12:00:50 +0100

    Pong terminado

--- Pong/Main.js
+++ Pong/Main.js
@@ -161,6 +161,32 @@
 		}
 	}
 
+	function hit(a,b){
+		//Revisa si a colisiona con b
+		var hit = false;
+		//Colsiones horizontales
+		if(b.x + b.width >= a.x && b.x < a.x + a.width)
+		{
+			//Colisiones verticales
+			if(b.y + b.height >= a.y && b.y < a.y + a.height)
+				hit = true;
+		}
+		//Colisión de a con b
+		if(b.x <= a.x && b.x + b.width >= a.x + a.width)
+		{
+			if(b.y <= a.y && b.y + b.height >= a.y + a.height)
+				hit = true;
+		}
+		//Colisión b con a
+		if(a.x <= b.x && a.x + a.width >= b.x + b.width)
+		{
+			if(a.y <= b.y && a.y + a.height >= b.y + b.height)
+				hit = true;
+		}
+		
+		return hit;
+	}
+
 	function draw(ctx,element){
 		
 		switch(element.kind){
